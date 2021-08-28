3e180bc941622e6ca9874a0c22b87d26d358df80
Pinki-851 <Pinkisaroj851@outlook.com>
2021-08-28 14:24:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import ProductView from "./components/productDetail/ProductView";
 import Product from "./components/productDetail/Product";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./store/rootReducer";
 
 function App() {
+  const store = createStore(rootReducer);
+
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Product} />
-          <Route exact path="/productdetail" component={ProductView} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ProductView} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 import rootReducer from "./store/rootReducer";
 
-const store = createStore(rootReducer);
-
-ReactDOM.render(
-  <>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
